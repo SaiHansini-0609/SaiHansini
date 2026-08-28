516aa01cb1a052f21e32dd5f724a7dba6679f26e
SaiHansini-0609 <kattasaihansini@gmail.com>
2026-08-28 12:54:13 +0530

    Create 2nd prog.java

---
+++ 2nd prog.java
@@ -0,0 +1,16 @@
+import java.util.Scanner()
+class Ex{
+public static void main(Sring args[]){
+System.out.println("Enter id name sal: ");
+Scanner sc=new Scanner(System.in);
+int id= sc.nextInt()
+String name=sc.next();
+float sal=sc.nextFloat();
+System.out.println("ID:"+id);
+System.out.println("NAME:"+name);
+System.out.println("SALARY:"+sal);
+}
+}
+
+
+
